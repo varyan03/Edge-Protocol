70d1e067dacfe85db64d4366dee58762a672a895
Aryan <av868600@gmail.com>
2025-10-20 18:39:34 +0530

    organized

---
+++ Character_Mov/script.js
@@ -0,0 +1,138 @@
+    const animationOptions = [
+        {value : 'idle', text: 'Idle'},
+        {value : 'jump', text: 'Jump'},
+        {value : 'fall', text: 'Fall'},
+        {value : 'run', text: 'Run'},
+        {value : 'dizzy', text: 'Dizzy'},
+        {value : 'sit', text: 'Sit'},
+        {value : 'roll', text: 'Roll'},
+        {value : 'bite', text: 'Bite'},
+        {value : 'ko', text: 'KO'},
+        {value : 'getHit', text: 'Get Hit'},
+    ]
+
+    const controlDev = document.querySelector('.control-dev');
+
+    const label = document.createElement('label');
+    label.htmlFor = 'animations';
+    label.textContent = 'Choose Animation: ';
+
+
+    const select = document.createElement('select');
+    select.name = 'animations';
+    select.id = 'animations';
+    
+
+    animationOptions.forEach(optionData => {
+        const option = document.createElement('option');
+        option.value = optionData.value;
+        option.textContent = optionData.text;
+        select.appendChild(option);
+    });
+
+    controlDev.appendChild(label);
+    controlDev.appendChild(select);
+   
+   
+   
+   let playerState = 'idle';
+    const dropdown = document.getElementById('animations');
+    dropdown.addEventListener('change', function(e){
+        playerState = e.target.value;
+    });
+    
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+        // --- FIX: Set width and height on the canvas element ---
+        // These dimensions are large enough to contain your drawing
+        // by default canvas is 300x150
+    const CANVAS_WIDTH = canvas.width = 600;
+    const CANVAS_HEIGHT = canvas.height = 600;
+
+    const img = new Image();
+    img.src = 'shadow_dog.png';
+        
+    let pos = 0;
+    const spriteWidth = 575;
+    const spriteHeight = 523;
+        
+
+    // to control animation speed
+    let gameFrame = 0;
+    const staggerFrames = 5; // Animation will now update every 10 frames instead of 5; higher the number slower the animation
+    const spriteAnimations = [];
+    const animationStates = [
+        {
+            name: 'idle',   
+            frames: 7,
+        },
+        {
+            name: 'jump',
+            frames: 7,
+        },
+        {
+            name: 'fall',
+            frames: 7,
+        },
+        {
+            name: 'run',    
+            frames: 9,
+        },
+        {
+            name: 'dizzy',
+            frames: 11,
+        },
+        {
+            name: 'sit',
+            frames: 5,
+        },
+        {
+            name: 'roll',
+            frames: 7,
+        },
+        {
+            name: 'bite',
+            frames: 7,
+        },
+        {
+            name: 'ko',
+            frames: 12,
+        },
+        {
+            name: 'getHit',
+            frames: 4,
+        }
+    ];  
+
+    // creating custom forEach for animations
+    animationStates.forEach((state, index) => {
+        let frames = {
+            loc: [],
+        } 
+        for(let j = 0; j < state.frames; j++){
+            let positionX = j * spriteWidth;
+            let positionY = index * spriteHeight;
+            frames.loc.push({x: positionX, y: positionY});
+        }   
+        spriteAnimations[state.name] = frames;
+    });
+
+    console.log(spriteAnimations);
+      
+    function animate(){
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length; // total frames are number of frames for the state; we using math.floor to round down(we only need integer values)
+            
+        let frameX = spriteWidth * position;
+        let frameY = spriteAnimations[playerState].loc[position].y;
+        console.log(frameX, frameY); // debugging purpose
+        ctx.drawImage(img, frameX , frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+            
+        gameFrame++;
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
+        
--- parallex/script.js
+++ parallex/script.js
@@ -18,16 +18,50 @@ backgroundLayer4.src = './backgroundLayers/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = './backgroundLayers/layer-5.png';
 
-let x = 0;// initial x coordinate
-let x2 = 2400; // second image x coordinate
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update() {
+        this.speed = gameSpeed * this.speedModifier;
+        if(this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed; // to make sure theere is no gap between the images
+        }
+        if(this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1,0.2);
+const layer2 = new Layer(backgroundLayer2,0.4);
+const layer3 = new Layer(backgroundLayer3,0.6);
+const layer4 = new Layer(backgroundLayer4,0.8);
+const layer5 = new Layer(backgroundLayer5,1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];  
 (function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0); // first image
-    ctx.drawImage(backgroundLayer4, x2, 0); // first image
-    if(x < -2400) x = 2400 + x2 - gameSpeed; // we are doing + x2 to avoid gap and - gameSpeed to avoid flicker and how is it working? the ans is is when we reset x to 2400, at that exact frame x2 is also moving left by game speed so to avoid flicker we subtract game speed
-    else x-= gameSpeed; // move to left by game speed
-    if(x2 < -2400) x2 = 2400 + x - gameSpeed; // we are doing + x to avoid gap and - gameSpeed to avoid flicker and how is it working? the ans is is when we reset x2 to 2400, at that exact frame x is also moving left by game speed so to avoid flicker we subtract game speed
-    else x2 -= gameSpeed; // move to left by game speed
+    gameObjects.forEach(object => {{
+        object.update();
+        object.draw();
+    }});
     requestAnimationFrame(animate);
      
 })();
