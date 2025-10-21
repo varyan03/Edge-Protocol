dadc65db2c37e64766e7057587733d4b2e7c6dcb
Aryan <av868600@gmail.com>
2025-10-21 20:30:51 +0530

    added movements

---
+++ input.js
@@ -0,0 +1,29 @@
+export class InputHandler {
+    constructor(){
+        this.keys = []; // contains all the keys being pressed  currently  
+        window.addEventListener('keydown', e => {
+            console.log(e);
+            if((e.key === 'ArrowDown' || 
+                e.key === 'ArrowUp' || 
+                e.key === 'ArrowLeft' || 
+                e.key === 'ArrowRight' ||
+                e.key === 'Enter') 
+                && this.keys.indexOf(e.key) === -1 ){
+                this.keys.push(e.key);
+            }
+            console.log(this.keys); 
+
+        });
+
+        window.addEventListener('keyup', e => {
+            if(e.key === 'ArrowDown' || 
+                e.key === 'ArrowUp' || 
+                e.key === 'ArrowLeft' || 
+                e.key === 'ArrowRight' ||
+                e.key === 'Enter'){
+                this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+            console.log(this.keys);
+        });
+    }
+}
